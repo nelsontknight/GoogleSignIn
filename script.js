8201dcad91073403caa0993d0997a9a5f674898c
Nelson Knight <nelson.knight92@gmail.com>
2019-09-30 18:08:37 -0600

    new files

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAjCKPaytT8KgyD6N8enxPI8TysBePq2jQ",
+  authDomain: "group8project1-cd703.firebaseapp.com",
+  databaseURL: "https://group8project1-cd703.firebaseio.com",
+  projectId: "group8project1-cd703",
+  storageBucket: "",
+  messagingSenderId: "98301800415",
+  appId: "1:98301800415:web:41977ef82112f835579e6a",
+  measurementId: "G-T74D3N65W8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 var provider = new firebase.auth.GoogleAuthProvider();
 
 firebase.auth().signInWithPopup(provider).then(function(result) {
